1c2d33a449dfdbc44e6541f6c66e1a30511614c7
kajalusadadiya <kajalvpatel07@gmail.com>
2021-12-28 23:30:57 +0000

    This is automation-pagefactoryweek-16

---
+++ src/test/java/com/automationpractice/testbase/TestBase.java
@@ -0,0 +1,23 @@
+package com.automationpractice.testbase;
+
+import com.automationpractice.propertyreader.PropertyReader;
+import com.automationpractice.utility.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase extends Utility {
+
+    String browser = PropertyReader.getInstance().getProperty("browser");
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(){
+        selectBrowser(browser);
+    }
+
+
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        closeBrowser();
+    }
+}
